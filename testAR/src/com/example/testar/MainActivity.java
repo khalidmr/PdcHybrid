81879c95aeb44883779f87f1ddf380a70dc702da
Pauline Montmayeur <pauline.montmayeur@insa-lyon.fr>
2015-01-17 15:43:15 +0100

    Changes AR activity : - Tree icon on GPS location - Shows detail on click

--- testAR/src/com/example/testar/MainActivity.java
+++ testAR/src/com/example/testar/MainActivity.java
@@ -2,24 +2,34 @@ package com.example.testar;
 
 
 import geo.GeoObj;
+import gl.Color;
 import gl.GL1Renderer;
 import gl.GLFactory;
+import gl.animations.AnimationFaceToCamera;
+import gl.scenegraph.MeshComponent;
 import system.ArActivity;
 import system.DefaultARSetup;
+import util.IO;
 import util.Vec;
 import worldData.World;
 
 import commands.Command;
+import commands.ui.CommandInUiThread;
+import commands.ui.CommandShowInfoScreen;
+import commands.ui.CommandShowToast;
 
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.view.InflateException;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 public class MainActivity extends ActionBarActivity{
@@ -51,29 +61,56 @@ public class MainActivity extends ActionBarActivity{
 				ArActivity.startWithSetup(MainActivity.this, new DefaultARSetup() {
 
 					@Override
-					public void addObjectsTo(GL1Renderer renderer, World world,
+					public void addObjectsTo(GL1Renderer renderer, final World world,
 							GLFactory objectFactory) {
 
-						GeoObj o = new GeoObj(45.781515 , 4.87212, 170);
-						o.setComp(objectFactory.newArrow());
-						o.setOnClickCommand(new Command() {
+						final GeoObj o = new GeoObj(45.77823,4.875113, 170);
+//						o.setComp(objectFactory.newArrow());
+//						o.setOnClickCommand(new CommandShowToast(context, "Click"));
+//						world.add(o);
+						
+						//show a tree picture at tree location
+						ImageView img = new ImageView(context);
+						img.setImageResource(R.drawable.arbre);
+						MeshComponent mesh = GLFactory.getInstance().newTexturedSquare(
+								"arbre", IO.loadBitmapFromView(img));
 
+						mesh.addChild(new AnimationFaceToCamera(camera, 0.5f));
+						mesh.setScale(new Vec(5, 5, 5));
+
+						final GeoObj tree = new GeoObj(o, mesh);
+						
+						mesh.setOnClickCommand(new CommandInUiThread() {
+							
 							@Override
-							public boolean execute() {
-
-								runOnUiThread(new Runnable() {
-
+							public void executeInUiThread() {
+								//show overlay
+								LayoutInflater inflater = LayoutInflater.from(context); // 1
+								View v = inflater.inflate(R.layout.tree_overlay, null);
+								MeshComponent meshOverlay = GLFactory.getInstance().newTexturedSquare(
+										"overlay", IO.loadBitmapFromView(v));
+								
+								meshOverlay.addChild(new AnimationFaceToCamera(camera, 0.5f));
+								meshOverlay.setScale(new Vec(5, 5, 5));
+								
+								final GeoObj overlay = new GeoObj(o, meshOverlay);
+								meshOverlay.setOnClickCommand(new Command() {
+									
 									@Override
-									public void run() {
-										Toast.makeText(context, "Click", Toast.LENGTH_SHORT).show();
-
+									public boolean execute() {
+										//remove overlay
+										world.remove(overlay);
+										world.add(tree);
+										return false;
 									}
 								});
-								return true;
-							}
+								world.remove(tree);
+								world.add(overlay);
+								}
 						});
-						world.add(o);
 
+						world.add(tree);
+						
 					}
 				});
 
